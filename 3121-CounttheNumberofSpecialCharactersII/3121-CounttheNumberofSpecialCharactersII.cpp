0dde8aab7dd5b08c5d059acc5f2fe6e34ae3a493
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-04-24 12:05:09 +0530

    Update file 4/24/2024, 12:05:08 PM

--- 3121-CounttheNumberofSpecialCharactersII/3121-CounttheNumberofSpecialCharactersII.cpp
+++ 3121-CounttheNumberofSpecialCharactersII/3121-CounttheNumberofSpecialCharactersII.cpp
@@ -1,7 +1,13 @@
     int numberOfSpecialChars(string word) {
-       map<char,int>m;
+      unordered_map<char,int>m;
        int i=0;
        for(auto x:word){
         if(x>='a'&&x<='z')
         m[x]=i;
+        else{
+            if(m.find(x)==m.end())m[x]=i;
+            // if(m[x]==0)m[x]=i;
+        }i++;
+       }
+       int ans=0;
 "
