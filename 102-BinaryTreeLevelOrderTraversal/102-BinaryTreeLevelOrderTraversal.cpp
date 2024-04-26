becb4bf2854e977cd688a57bc4628be257a7cbc0
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-04-26 19:41:44 +0530

    Create file

---
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,10 @@
+        mq.push(root);
+        vector<vector<int>>ans;
+        queue<TreeNode*>mq;
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        while(mq.size()>0){
+            int size=mq.size();
+    
+public:
+        if(root!=NULL)
+[
