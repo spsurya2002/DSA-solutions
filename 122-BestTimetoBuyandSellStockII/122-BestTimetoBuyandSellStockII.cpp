2aa55d87bc5aa57f73000c9d6009b71b717daeaa
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-05-03 08:59:41 +0530

    Update file 5/3/2024, 8:59:40 AM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.cpp
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     int maxProfit(vector<int>& prices) {
-        int ans=0;
+       unsigned int ans=0;
         for(int i=0;i<prices.size()-1;i++){
             if(prices[i+1]-prices[i]>=0)ans+=(prices[i+1]-prices[i]);
         }return ans;
