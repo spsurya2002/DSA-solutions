49adfeebfc16a314db578edad0dce02864720c1a
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-05-06 20:36:33 +0530

    Create file

---
+++ EasyC++solution||Bruteforce||O(n^2)solution/EasyC++solution||Bruteforce||O(n^2)solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        int n=satisfaction.size();
+        sort(satisfaction.begin(),satisfaction.end());
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int sum=0,cnt=1;
+            for(int j=i;j<n;j++){
+                sum+=cnt*satisfaction[j];
+                cnt++;
+            }ans=max(ans,sum);
+        }return ans;
+
+    }
+};
+[-1,-8,0,5,-7]
