8998c51a3f418bd347f9bc9ae67f39b91b86f3f3
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-05-06 20:33:17 +0530

    Update file 5/6/2024, 8:33:16 PM

--- 1402-ReducingDishes/1402-ReducingDishes.cpp
+++ 1402-ReducingDishes/1402-ReducingDishes.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
-         int n=satisfaction.size();
+        int n=satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end());
-        unsigned int ans=0;
+        int ans=0;
         for(int i=0;i<n;i++){
-            unsigned sum=0,cnt=1;
+            int sum=0,cnt=1;
             for(int j=i;j<n;j++){
                 sum+=cnt*satisfaction[j];
                 cnt++;
