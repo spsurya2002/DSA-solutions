bbec81b0c8417dbbb7b92731d59f008713fd4d2d
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-05-07 09:03:49 +0530

    Update file 5/7/2024, 9:03:48 AM

--- 2816-DoubleaNumberRepresentedasaLinkedList/2816-DoubleaNumberRepresentedasaLinkedList.cpp
+++ 2816-DoubleaNumberRepresentedasaLinkedList/2816-DoubleaNumberRepresentedasaLinkedList.cpp
@@ -1,5 +1,24 @@
-        num=addStrings(num,num);
-        
-        ListNode* ans = new ListNode(num[0]-'0');
-        temp = ans;
+}
+    ListNode* doubleIt(ListNode* head) {
+        ListNode* temp=head;
+    return result;
+    
+    }
+        result = std::to_string(sum) + result;
+        sum %= 10;
+        carry = sum / 10;
+        }
+            j--;
+            sum += num2[j] - '0';
+        if (j >= 0) {
+        }
+            i--;
+            sum += num1[i] - '0';
+        if (i >= 0) {
+        int sum = carry;
+    while (i >= 0 || j >= 0 || carry) {
+    
+    int j = num2.size() - 1;
+    int i = num1.size() - 1;
+    int carry = 0;
 [
