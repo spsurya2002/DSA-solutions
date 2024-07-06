64962670c1ac38ba797cf1c507c2be011a696f2a
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-06 10:06:00 +0530

    Create file

---
+++ 2582-PassthePillow/2582-PassthePillow.cpp
@@ -0,0 +1,11 @@
+       cout<<r1<<" "<<d1<<endl;
+        if(r1==0){
+            if(d1%2==0)return 1;
+            else return n;
+        }
+        if(d1%2==0)
+        return r1+1;
+        return n-(r1);
+    }
+        int d1=time/(n-1);
+4
