cbb12c2e2991d8a3db8b28547600b2e26f549aed
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-06 10:06:36 +0530

    Update file 7/6/2024, 10:06:36 AM

--- 2582-PassthePillow/2582-PassthePillow.cpp
+++ 2582-PassthePillow/2582-PassthePillow.cpp
@@ -1,9 +1,10 @@
-       cout<<r1<<" "<<d1<<endl;
-        
         if(d1%2==0)
         return r1+1;
         return n-(r1);
-    }
         int d1=time/(n-1);
+        int r1=time%(n-1);
+    int passThePillow(int n, int time) {
+public:
+    }
 };
 4
