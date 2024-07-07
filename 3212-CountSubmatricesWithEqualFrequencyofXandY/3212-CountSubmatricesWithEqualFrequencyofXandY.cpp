c2e2aa0d975288b5b24a465f255ec406903901f1
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-07 10:32:57 +0530

    Create file

---
+++ 3212-CountSubmatricesWithEqualFrequencyofXandY/3212-CountSubmatricesWithEqualFrequencyofXandY.cpp
@@ -0,0 +1,9 @@
+            }
+                dp[i][j].first=x;dp[i][j].second=y;
+                if(grid[i][j]=='Y')y++;
+                if(grid[i][j]=='X')x++;
+            for(int j=0;j<n;j++){
+            int x=0,y=0;
+        for(int i=0;i<m;i++){
+        
+[
