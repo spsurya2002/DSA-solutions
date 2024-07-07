6c4013f68c4ca515a8c3ddaf0b44c3a24c6d8600
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-07 10:37:44 +0530

    Update file 7/7/2024, 10:37:44 AM

--- 3212-CountSubmatricesWithEqualFrequencyofXandY/3212-CountSubmatricesWithEqualFrequencyofXandY.cpp
+++ 3212-CountSubmatricesWithEqualFrequencyofXandY/3212-CountSubmatricesWithEqualFrequencyofXandY.cpp
@@ -1,9 +1,19 @@
             }
-                dp[i][j].first=x;dp[i][j].second=y;
-                if(grid[i][j]=='Y')y++;
-                if(grid[i][j]=='X')x++;
-            for(int j=0;j<n;j++){
-            int x=0,y=0;
+        }
+        int ans=0;
         for(int i=0;i<m;i++){
-        
+            for(int j=0;j<n;j++){
+                if(i==0){
+                    if(dp[0][j].first>=1){
+                     if(dp[0][j].first==dp[0][j].second)ans++;
+                     }
+            }
+                {dp[i][j].first=dp[i][j].first+dp[i-1][j].first;
+                dp[i][j].second=dp[i][j].second+dp[i-1][j].second;
+               if(dp[i][j].first>=1){
+                   if(dp[i][j].first==dp[i][j].second)ans++;
+               }}
+            }
+        }
+            else
 [
