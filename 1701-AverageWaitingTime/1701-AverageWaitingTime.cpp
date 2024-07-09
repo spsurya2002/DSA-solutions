09f8673768a0993f1f0eef78b8b6bb2f48f52e16
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-09 10:46:47 +0530

    Update file 7/9/2024, 10:46:47 AM

--- 1701-AverageWaitingTime/1701-AverageWaitingTime.cpp
+++ 1701-AverageWaitingTime/1701-AverageWaitingTime.cpp
@@ -3,4 +3,13 @@ public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         double finishTime=customers[0][0]+customers[0][1];
         double sumTime=finishTime-customers[0][0];
+        for(int i=1;i<customers.size();i++){
+            if(finishTime<customers[i][0])finishTime=customers[i][0];
+            finishTime+=customers[i][1];
+            sumTime+=(finishTime-customers[i][0]);
+        }
+        return ans;
+    }
+        double ans =  sumTime/(customers.size());
+};
 [
