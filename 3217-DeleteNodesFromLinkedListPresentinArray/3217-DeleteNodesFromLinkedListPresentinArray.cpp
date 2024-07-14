b6c5dd1f4bf4934dd60b50113af58d8ae5f8c844
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-14 15:05:38 +0530

    Update file 7/14/2024, 3:05:38 PM

--- 3217-DeleteNodesFromLinkedListPresentinArray/3217-DeleteNodesFromLinkedListPresentinArray.cpp
+++ 3217-DeleteNodesFromLinkedListPresentinArray/3217-DeleteNodesFromLinkedListPresentinArray.cpp
@@ -1,5 +1,18 @@
-    last->next = new_node;
-}
-
-    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+       ListNode*  ans = nullptr;
+       ListNode*  last = nullptr;
+       while(temp!=NULL){
+        int data=temp->val;
+        if(!(binary_search(nums.begin(),nums.end(),data))){
+           ListNode* newNode = new ListNode(data);
+           if(ans==nullptr){ans=newNode;last=newNode;}
+           else{
+            last->next = newNode;
+            last = newNode;
+           }
+         }temp = temp->next;
+       } 
+       return ans;
+    }
+    
+};
 [
