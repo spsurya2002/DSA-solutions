64c4a0c617a69e7ed4a0655082cb630730f0ad3c
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-15 14:10:38 +0530

    Update file 7/15/2024, 2:10:37 PM

--- 474-OnesandZeroes/474-OnesandZeroes.cpp
+++ 474-OnesandZeroes/474-OnesandZeroes.cpp
@@ -1,21 +1,16 @@
-         
+         return 0;
         }
         if(dp[z][o][i]!=-1)return dp[z][o][i];
         vector<int> v=calc01(strs[i]);
         int zero = v[0];
         int one = v[1];
-
-        int ans1=0;
-        if(z+zero<=m&&o+one<=n)
-        ans1=find01(strs,m,n,z+zero,o+one,i+1)+1;
-        int ans2=find01(strs,m,n,z,o,i+1);
-        return dp[z][o][i]=max(ans1,ans2);
-        
+        if(zero+z<=m&&o+one<=n)
+        return dp[z][o][i]=max(find01(strs,m,n,z+zero,o+one,i+1)+1,find01(strs,m,n,z,o,i+1));
+        return dp[z][o][i]=find01(strs,m,n,z,o,i+1);
     }
     int findMaxForm(vector<string>& strs, int m, int n) {
         fill();
         int ans = find01(strs,m,n,0,0,0);
         return ans;
     }
-};
 [
