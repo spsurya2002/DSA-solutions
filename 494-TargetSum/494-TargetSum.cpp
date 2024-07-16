2e9d2f330418cdf379b6f82f7badae10344fc5c8
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-16 19:43:23 +0530

    Update file 7/16/2024, 7:43:22 PM

--- 494-TargetSum/494-TargetSum.cpp
+++ 494-TargetSum/494-TargetSum.cpp
@@ -1,15 +1,9 @@
-    
+    int find(vector<int>& nums,int target,int cur_sum,int i){
 public:
 class Solution {
-    vector<int> find(vector<int>& nums,int i){
-        if(i==nums.size()-1){
-            return {-1*nums[i],nums[i]};
+        if(i>=nums.size()&&cur_sum==target)return 1;
+        if(i>=nums.size()){
+            return 0;
         }
-        vector<int> res = find(nums,i+1);
-        vector<int> myres;
-        for(auto x:res){
-            myres.push_back(x+nums[i]);
-            myres.push_back(x-nums[i]);
-        }
-        return myres;
+        int ans1=find(nums,target,cur_sum+nums[i],i+1);
 [
