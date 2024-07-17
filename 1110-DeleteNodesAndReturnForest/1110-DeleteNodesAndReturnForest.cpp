88a49c96204eec16622f47fa7dc505e5631dc8d8
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-17 10:51:18 +0530

    Create file

---
+++ 1110-DeleteNodesAndReturnForest/1110-DeleteNodesAndReturnForest.cpp
@@ -0,0 +1,11 @@
+           if(s.find(root->val)!=s.end()){
+            if(root->left!=NULL)
+           root->right= del(root->right,s,ans);
+           root->left= del(root->left,s,ans);
+        if(root==NULL)return NULL;
+    TreeNode* del(TreeNode* root,unordered_set<int>s,vector<TreeNode*>&ans){
+public:
+class Solution {
+ */
+ * };
+[
