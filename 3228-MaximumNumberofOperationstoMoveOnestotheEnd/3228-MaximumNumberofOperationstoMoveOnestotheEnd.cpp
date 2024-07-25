e45ccfeb960e1cb3f13ff2bec8cf1831f25720ae
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-25 12:53:32 +0530

    Update file 7/25/2024, 12:53:30 PM

--- 3228-MaximumNumberofOperationstoMoveOnestotheEnd/3228-MaximumNumberofOperationstoMoveOnestotheEnd.cpp
+++ 3228-MaximumNumberofOperationstoMoveOnestotheEnd/3228-MaximumNumberofOperationstoMoveOnestotheEnd.cpp
@@ -1,14 +1,19 @@
            }
            if(s[i]=='1')o++;
         }
-        if(s[s.length()-1]=='0')ans+=o;
+        if(s.back()=='0')ans+=o;
          return ans;
             if(s[i-1]=='0')ans+=o;
            if(i>0&&s[i]=='1'){
            
         for(int i=0;i<s.length();i++){
     }
+        bool b=false;
+        int ans=0,o=0;
+    int maxOperations(string s) {
+public:
 };
+class Solution {
 "0111010"
 "00111"
 "010010"
