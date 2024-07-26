53aaec1c7cdf77d35784590c44b731333aa18551
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-26 14:52:10 +0530

    Create file

---
+++ 3223-MinimumLengthofStringAfterOperations/3223-MinimumLengthofStringAfterOperations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        unordered_map<char,int>m;
+        for(auto x:s)m[x]++;
+        int ans=0;
+        for(auto x:m){
+            cout<<x.first<<" "<<x.second<<endl;
+            if(x.second>2&&(x.second%2==0)){ans+=2;}
+            else if(x.second>2&&(x.second%2==1)){ans+=1;}
+            else ans+=x.second;
+        }
+"abaacbcbb"
+"aa"
+c 2
+b 4
+a 3
+
+a 2
+
+5
+2
+5
+2
+"
