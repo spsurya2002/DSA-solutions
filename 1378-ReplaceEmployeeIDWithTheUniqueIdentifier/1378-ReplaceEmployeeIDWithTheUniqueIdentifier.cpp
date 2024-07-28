3cd379aed20c89cf73a22032bbd7ed6ed5525a19
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-28 13:55:22 +0530

    Create file

---
+++ 1378-ReplaceEmployeeIDWithTheUniqueIdentifier/1378-ReplaceEmployeeIDWithTheUniqueIdentifier.cpp
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+SELECT unique_id, name FROM Employees 
+LEFT JOIN EmployeeUNI
+ON Employees.id=EmployeeUNI.id;
+{"headers":{"Employees":["id","name"],"EmployeeUNI":["id","unique_id"]},"rows":{"Employees":[[1,
+"Alice"],[7,"Bob"],[11,"Meir"],[90,"Winston"],[3,"Jonathan"]],"EmployeeUNI":[[3,1],[11,2],[90,3]]}}
+| unique_id | name     |
+| --------- | -------- |
+| null      | Alice    |
+| null      | Bob      |
+| 2         | Meir     |
+| 3         | Winston  |
+| 1         | Jonathan |
+| unique_id | name     |
+| --------- | -------- |
+| null      | Alice    |
+| null      | Bob      |
+| 2         | Meir     |
+| 3         | Winston  |
+| 1         | Jonathan |
+[
