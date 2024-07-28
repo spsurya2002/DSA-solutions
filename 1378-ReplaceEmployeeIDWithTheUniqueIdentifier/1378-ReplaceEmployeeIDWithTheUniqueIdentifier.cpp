4c8127c9c8a7a38c6d5c4312a02130bc81a0b42d
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-28 13:55:47 +0530

    Update file 7/28/2024, 1:55:47 PM

--- 1378-ReplaceEmployeeIDWithTheUniqueIdentifier/1378-ReplaceEmployeeIDWithTheUniqueIdentifier.cpp
+++ 1378-ReplaceEmployeeIDWithTheUniqueIdentifier/1378-ReplaceEmployeeIDWithTheUniqueIdentifier.cpp
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-SELECT unique_id, name FROM Employees 
-LEFT JOIN EmployeeUNI
-ON Employees.id=EmployeeUNI.id;
+SELECT unique_id, name FROM Employees as E
+LEFT JOIN EmployeeUNI as EU
+ON E.id=EU.id;
 {"headers":{"Employees":["id","name"],"EmployeeUNI":["id","unique_id"]},"rows":{"Employees":[[1,
 "Alice"],[7,"Bob"],[11,"Meir"],[90,"Winston"],[3,"Jonathan"]],"EmployeeUNI":[[3,1],[11,2],[90,3]]}}
 | unique_id | name     |
