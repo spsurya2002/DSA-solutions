8562fec1ea753ed55a5029f0cea609bc03df1529
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-07-28 13:50:04 +0530

    Create file

---
+++ 1683-InvalidTweets/1683-InvalidTweets.cpp
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT tweet_id FROM Tweets
+WHERE LENGTH(content)>15;
+{"headers":{"Tweets":["tweet_id","content"]},"rows":{"Tweets":[[1,"Vote for Biden"],[2,"Let us 
+make America great again!"]]}}
+| tweet_id |
+| -------- |
+| 2        |
+| tweet_id |
+| -------- |
+| 2        |
+[
