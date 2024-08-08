d3621036321e487ae973185fc5a59cf14aec1255
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-08 15:36:21 +0530

    Update file 8/8/2024, 3:36:19 PM

--- 550-GamePlayAnalysisIV/550-GamePlayAnalysisIV.cpp
+++ 550-GamePlayAnalysisIV/550-GamePlayAnalysisIV.cpp
@@ -1,19 +1,17 @@
-    GROUP BY player_id
-),
-SecondDayLogin AS (
-    SELECT a.player_id
-    FROM Activity a
-    JOIN FirstLogin f ON a.player_id = f.player_id
-    WHERE DATEDIFF(a.event_date, f.first_login_date) = 1
-)
-SELECT ROUND(COUNT(DISTINCT s.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
-    FROM Activity
-    SELECT player_id, MIN(event_date) AS first_login_date
-WITH FirstLogin AS (
+-- FROM SecondDayLogin AS s;
 
-FROM SecondDayLogin AS s;
-
-
+SELECT 
+    ROUND(COUNT(DISTINCT a1.player_id) / 
+    (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM 
+    Activity a1
+JOIN 
+    (SELECT player_id, MIN(event_date) AS first_login_date
+     FROM Activity
+     GROUP BY player_id) AS first_login
+ON 
+    a1.player_id = first_login.player_id 
+    AND DATEDIFF(a1.event_date, first_login.first_login_date) = 1;
 {"headers":{"Activity":["player_id","device_id","event_date","games_played"]},"rows":{"Activity":[[1,2,
 "2016-03-01",5],[1,2,"2016-03-02",6],[2,3,"2017-06-25",1],[3,1,"2016-03-02",0],[3,4,"2018-07-03",5]]}}
 {"headers":{"Activity":["player_id","device_id","event_date","games_played"]},"rows":{"Activity":[[5,17,
