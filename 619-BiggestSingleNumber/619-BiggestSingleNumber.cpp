4041156b1f7ecff277f4297b1bb115762e87ea22
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-09 15:58:30 +0530

    Update file 8/9/2024, 3:58:28 PM

--- 619-BiggestSingleNumber/619-BiggestSingleNumber.cpp
+++ 619-BiggestSingleNumber/619-BiggestSingleNumber.cpp
@@ -1,18 +1,10 @@
-SELECT 
-    CASE 
-        WHEN COUNT(*) = 0 THEN NULL
-        ELSE (
-            SELECT num
-            FROM MyNumbers
-            GROUP BY num
-            HAVING COUNT(num) = 1
-            ORDER BY num DESC
-            LIMIT 1
-        )
-    END AS num
+# Write your MySQL query statement below
+SELECT MAX(num) as num 
+FROM(SELECT num
 FROM MyNumbers
-LIMIT 1;
-
+GROUP by num
+HAVING COUNT(num)=1) as newMYnumber
+;
 {"headers":{"MyNumbers":["num"]},"rows":{"MyNumbers":[[8],[8],[3],[3],[1],[4],[5],[6]]}}
 {"headers":{"MyNumbers":["num"]},"rows":{"MyNumbers":[[8],[8],[7],[7],[3],[3],[3]]}}
 | num |
