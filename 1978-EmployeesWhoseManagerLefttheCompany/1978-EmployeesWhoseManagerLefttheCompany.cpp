1fc1efce38c7d417a8c9c396bfc7affc6474c468
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-12 12:13:20 +0530

    Create file

---
+++ 1978-EmployeesWhoseManagerLefttheCompany/1978-EmployeesWhoseManagerLefttheCompany.cpp
@@ -0,0 +1,23 @@
+    FROM Employees
+    WHERE salary < 30000
+) AS el30
+WHERE el30.manager_id NOT IN (
+    SELECT employee_id
+    SELECT employee_id, manager_id
+FROM (
+SELECT employee_id
+# Write your MySQL query statement below
+    FROM Employees
+) 
+ORDER BY employee_id ;
+
+{"headers":{"Employees":["employee_id","name","manager_id","salary"]},"rows":{"Employees":[[3,
+"Mila",9,60301],[12,"Antonella",null,31000],[13,"Emery",null,67084],[1,"Kalel",11,21241],[9,
+"Mikaela",null,50937],[11,"Joziah",6,28485]]}}
+| employee_id |
+| ----------- |
+| 11          |
+| employee_id |
+| ----------- |
+| 11          |
+[
