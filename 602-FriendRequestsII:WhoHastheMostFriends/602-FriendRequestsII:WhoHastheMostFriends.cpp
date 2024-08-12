a41fbdc41aad0505fe41034a64b9a4097e8542bd
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-12 13:51:50 +0530

    Update file 8/12/2024, 1:51:47 PM

--- 602-FriendRequestsII:WhoHastheMostFriends/602-FriendRequestsII:WhoHastheMostFriends.cpp
+++ 602-FriendRequestsII:WhoHastheMostFriends/602-FriendRequestsII:WhoHastheMostFriends.cpp
@@ -1,7 +1,17 @@
 ORDER BY num DESC
 LIMIT 1
-;
 GROUP BY id 
+    GROUP BY accepter_id) as new_table
+    UNION ALL
+    SELECT accepter_id as id, COUNT(accepter_id) as cnt
+    FROM RequestAccepted
+    GROUP BY requester_id
+    FROM RequestAccepted
+FROM (
+SELECT id,SUM(cnt) as num
+# Write your MySQL query statement below
+    SELECT requester_id as id, COUNT(requester_id) as cnt
+;
 {"headers":{"RequestAccepted":["requester_id","accepter_id","accept_date"]},"rows":
 {"RequestAccepted":[[1,2,"2016/06/03"],[1,3,"2016/06/08"],[2,3,"2016/06/08"],[3,4,"2016/06/
 09"]]}}
