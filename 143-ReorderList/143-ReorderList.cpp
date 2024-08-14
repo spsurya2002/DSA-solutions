0ba5a69223b7a952fd15ccb5cba74dab134eba65
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-14 13:18:08 +0530

    Update file 8/14/2024, 1:18:08 PM

--- 143-ReorderList/143-ReorderList.cpp
+++ 143-ReorderList/143-ReorderList.cpp
@@ -1,14 +1,14 @@
-    void reorderList(ListNode* head) {
-       vector<int>v;
-       ListNode* temp=head;
        while(temp!=nullptr){
         v.push_back(temp->val);
         temp=temp->next;
        }
        int i=0,j=v.size()-1;
-       while(i<=j){
+       while(i<j){
         head->val=v[i];i++;head=head->next;
         if(i<j){
+        head->val=v[j];
+        head=head->next;
+        j--;}
 [1,2,3,4]
 [1,2,3,4,5]
 [1,4,2,3]
