da070393458477a0d6a07c76c13b8a17edec0914
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-14 13:27:10 +0530

    Create file

---
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.cpp
@@ -0,0 +1,17 @@
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+      ListNode* fast=head;
+      ListNode* slow=head;
+[1,2,3,4,5]
+[1,2,3,4,5,6]
+[3,4,5]
+[4,5,6]
+[3,4,5]
+[4,5,6]
+[
