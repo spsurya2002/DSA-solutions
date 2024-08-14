fda6a621238f6bfe31cc4331d253d7cfde6cf0a7
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-14 13:17:54 +0530

    Create file

---
+++ 143-ReorderList/143-ReorderList.cpp
@@ -0,0 +1,18 @@
+    void reorderList(ListNode* head) {
+       vector<int>v;
+       ListNode* temp=head;
+       while(temp!=nullptr){
+        v.push_back(temp->val);
+        temp=temp->next;
+       }
+       int i=0,j=v.size()-1;
+       while(i<=j){
+        head->val=v[i];i++;head=head->next;
+        if(i<j){
+[1,2,3,4]
+[1,2,3,4,5]
+[1,4,2,3]
+[1,5,2,4,3]
+[1,4,2,3]
+[1,5,2,4,3]
+[
