95f9c7ae2062dc26461a532f14f2f4738b877a7b
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-20 13:14:39 +0530

    Create file

---
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.cpp
@@ -0,0 +1,21 @@
+        queue<TreeNode*>mq;
+    if(node==nullptr)return;
+void levelOrderTraversal(TreeNode* node){
+  vector<int>v;
+public:
+class Solution {
+ */
+ * };
+[1,2,3,null,5,null,4]
+[1,null,3]
+[]
+1 2 3 5 4 1 3 4 
+1 3 1 3 
+
+[1,3,4]
+[1,3]
+[]
+[1,3,4]
+[1,3]
+[]
+[
