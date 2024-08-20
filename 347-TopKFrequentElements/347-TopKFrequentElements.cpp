aaf458ed47d29f542f2aec10dd1e32337c9624b7
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-20 11:27:46 +0530

    Create file

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -0,0 +1,18 @@
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int>m;
+        for(auto x:nums){
+            m[x]++;
+        }
+public:
+class Solution {
+        vector<pair<int,int>>vp;
+        for(auto x:m)
+[1,1,1,2,2,3]
+2
+[1]
+1
+[1,2]
+[1]
+[1,2]
+[1]
+[
