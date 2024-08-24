602a8b7bc8ed072ee828bed62f3d46c11a3a4f95
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-24 14:03:51 +0530

    Create file

---
+++ 24-SwapNodesinPairs/24-SwapNodesinPairs.cpp
@@ -0,0 +1,16 @@
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head->next==nullptr)return head;
+        if(head==nullptr)return head;
+[1,2,3,4]
+[]
+[1]
+[2,1,4,3]
+[]
+[1]
+[2,1,4,3]
+[]
+[1]
+[
