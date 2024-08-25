3efcec4afe315c17fa8e06425b84b8e769241a4c
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-25 13:34:08 +0530

    Update file 8/25/2024, 1:34:07 PM

--- 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
@@ -1,11 +1,14 @@
- 
- 
+        ListNode* mid = head;
+        ListNode* temp=head;
+    bool isPalindrome(ListNode* head) {
+    }
+        }
+            head=head->next;
+            cout<<head->val<<" ";
+        while(head!=nullptr){
 [1,2,2,1]
 [1,2]
 [1,3,2,4,3,2,1]
-1 2 
-2 
-1 2 3 
 true
 false
 false
