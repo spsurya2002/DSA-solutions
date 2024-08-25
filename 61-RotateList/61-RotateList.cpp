d33343844da49d53c4034bd8d7f6e50471d7502d
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-08-25 15:28:27 +0530

    Update file 8/25/2024, 3:28:26 PM

--- 61-RotateList/61-RotateList.cpp
+++ 61-RotateList/61-RotateList.cpp
@@ -1,22 +1,20 @@
-        if(k==0)return head;
-        if(head==nullptr||head->next==nullptr)return head;
-    ListNode* rotateRight(ListNode* head, int k) {
-    }
-        }return l;
-            head=head->next;
-            l++;
-        while(head){
-        int l=0;
-public:
     int len(ListNode* head){
-class Solution {
- */
- * };
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode *next;
- *     int val;
+        int l=0;
+        while(head){
+            l++;
+            head=head->next;
+        }return l;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==nullptr||head->next==nullptr)return head;
+        if(k==0)return head;
+        int l = len(head);
+        k=k%l;
+        if(k==0)return head;
+        k=l-k;
+
+        ListNode* fh = head;
+        ListNode* ft = fh;
 [1,2,3,4,5]
 2
 [0,1,2]
@@ -31,26 +29,6 @@ class Solution {
 3
 [1,2]
 2
-1 2 3 4 5 
-3 4 5 
-4 5 
-5 
-
-0 1 2 
-1 2 
-2 
-2 
-
-
-
-2 
-
-1 2 3 4 5 6 7 8 9 
-6 7 8 9 
-7 8 9 
-9 
-
-
 [4,5,1,2,3]
 [2,0,1]
 []
