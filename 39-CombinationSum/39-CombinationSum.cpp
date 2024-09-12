8ffcc20f8384dd23350b9e4d1a3d69d5fcfc7922
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-09-12 13:56:03 +0530

    Create file

---
+++ 39-CombinationSum/39-CombinationSum.cpp
@@ -0,0 +1,19 @@
+            ans.push_back(v);
+        if(sum==target){
+    void find(vector<int>& num, int i,int sum, vector<int>v,int target){
+    vector<vector<int>>ans;
+public:
+class Solution {
+[2,3,6,7]
+7
+[2,3,5]
+8
+[2]
+1
+[[2,2,3],[7]]
+[[2,2,2,2],[2,3,3],[3,5]]
+[]
+[[2,2,3],[7]]
+[[2,2,2,2],[2,3,3],[3,5]]
+[]
+[
