7117ecbf66f529f397330ed93ec4e6422e2424fd
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-09-13 11:19:51 +0530

    Create file

---
+++ 131-PalindromePartitioning/131-PalindromePartitioning.cpp
@@ -0,0 +1,15 @@
+            myres.push_back(temp);
+        }
+
+        return myres;
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<deque<string>> ds = find(s);
+"aab"
+"a"
+[["a","a","b"],["aa","b"]]
+[["a"]]
+[["a","a","b"],["aa","b"]]
+[["a"]]
+"
