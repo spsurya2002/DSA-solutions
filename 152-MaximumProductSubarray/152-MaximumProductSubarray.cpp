44231a8f8aa2aa8a1a851d3bb31e6bb6ab77d577
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-09-16 00:51:22 +0530

    Create file

---
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& arr) {
+        int n = arr.size(); //size of array.
+
+    int pre = 1, suff = 1;
+    int ans = INT_MIN;
+[2,3,-2,4]
+[-2,0,-1]
+[0,0,0,0]
+6
+0
+0
+6
+0
+0
+[
