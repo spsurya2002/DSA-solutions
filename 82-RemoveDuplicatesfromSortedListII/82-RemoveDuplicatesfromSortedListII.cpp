b7d143cab790dbeae97ef8ce551feb019e7a4762
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-10-08 13:18:53 +0530

    Update file 10/8/2024, 1:18:53 PM

--- 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.cpp
+++ 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.cpp
@@ -1,16 +1,20 @@
         }
 
-        ListNode* temp1=new ListNode();
-        ListNode* ans=temp1;
+        temp=new ListNode();
+        ListNode* ans=temp;
         for(auto x:st){
             ListNode* nnd=new ListNode(x);
-            temp1->next=nnd;
-            temp1=temp1->next;
+            temp->next=nnd;
+            temp=temp->next;
         }
         return ans->next;
     }
 };
             if(x.second==1)st.insert(x.first);
+        for(auto x:mp){
+        }
+            temp=temp->next;
+           
 [1,2,3,3,4,4,5]
 [1,1,1,2,3]
 [1,2,5]
