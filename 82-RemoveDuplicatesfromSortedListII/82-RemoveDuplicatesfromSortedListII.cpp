03300d126c327d317a6294b3e3373a9f11da2515
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-10-08 13:28:46 +0530

    Update file 10/8/2024, 1:28:46 PM

--- 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.cpp
+++ 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.cpp
@@ -1,26 +1,23 @@
+public:
+class Solution {
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(-101, head);
+        ListNode* prev = dummy;  
+        ListNode* current = head;
         
         while (current) {
             if (current->next && current->val == current->next->val) {
-        ListNode* current = head;
-        ListNode* prev = dummy;  
-        ListNode* dummy = new ListNode(0, head);
-    ListNode* deleteDuplicates(ListNode* head) {
-public:
-class Solution {
-            }
-            current = current->next;
                 while (current->next && current->val == current->next->val) {
                     current = current->next;
                 }
                 prev->next = current->next;
             } else {
                 prev = prev->next;
-        }
-        
+            }
 [1,2,3,3,4,4,5]
 [1,1,1,2,3]
-[0,1,2,3,3,4,4,5]
-[0,1,1,1,2,3]
+[-101,1,2,3,3,4,4,5]
+[-101,1,1,1,2,3]
 [1,2,5]
 [2,3]
 [
