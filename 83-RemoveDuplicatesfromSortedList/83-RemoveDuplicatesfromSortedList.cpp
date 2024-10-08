f97ba943820cf113c7949ecee3da2c946ee66438
spsurya2002 <113915327+spsurya2002@users.noreply.github.com>
2024-10-08 13:21:03 +0530

    Create file

---
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
@@ -0,0 +1,25 @@
+        }
+        
+
+        temp=new ListNode();
+            st.insert(temp->val);
+        while(temp!=nullptr){
+        ListNode* temp=head;
+        set<int>st;
+    ListNode* deleteDuplicates(ListNode* head) {
+public:
+class Solution {
+        ListNode* ans=temp;
+        for(auto x:st){
+            ListNode* nnd=new ListNode(x);
+            temp->next=nnd;
+            temp=temp->next;
+        }
+            temp=temp->next;
+[1,1,2]
+[1,1,2,3,3]
+[1,2]
+[1,2,3]
+[1,2]
+[1,2,3]
+[
